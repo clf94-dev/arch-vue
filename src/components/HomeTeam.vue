eb3784f509face80402ffa99e3a83d5cd80a1319
clf94-dev <clf94@hotmail.es>
2020-11-20 07:58:49 +0100

    added HomeTeam struct

--- src/components/HomeTeam.vue
+++ src/components/HomeTeam.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-<h1> hometeam component</h1>
-    </div>
+  <b-container fluid>
+    <b-col class="team-cont">
+      <img :src="teamD" alt="team" />
+      <b-col class="team-text">
+        <h3>Small team, big ideas</h3>
+        <button @click="goToAbout()">
+          About Us <img :src="arrow" alt="arrow" />
+        </button>
+      </b-col> </b-col
+  ></b-container>
 </template>
 
 <script>
-    export default {
-        name: 'HomeTeam'
-    }
+export default {
+  name: "HomeTeam",
+  data() {
+    return {
+      teamD: require("@/assets/images/home/desktop/image-small-team.jpg"),
+      arrow: require("@/assets/images/icons/icon-arrow.svg"),
+    };
+  },
+  methods: {
+    goToAbout() {
+      this.$router.push("/about");
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
