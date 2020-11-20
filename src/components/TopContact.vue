ea90ff31a6d4f27a1a4e8f504b862e9108cf4795
clf94-dev <clf94@hotmail.es>
2020-11-20 09:33:15 +0100

    added TopContact struct

--- src/components/TopContact.vue
+++ src/components/TopContact.vue
@@ -1,15 +1,31 @@
 <template>
-    <div>
-<h1> topcontact component</h1>
-    </div>
+  <b-container fluid class="top-section-cont">
+    <b-row>
+      <b-col class="img-col" xs="12" sm="12" md="6" lg="6">
+        <img :src="topContactD" alt="About"
+      /></b-col>
+      <b-col class="text-col" xs="12" sm="12" md="6" lg="6">
+        <h1>Contact</h1>
+        <h3>Tell us about your project</h3>
+        <p>
+          We’d love to hear more about your project. Please, leave a message
+          below or give us a call. We have two offices, one in Texas and one in
+          Tennessee. If you find yourself nearby, come say hello!
+        </p>
+      </b-col>
+    </b-row></b-container
+  >
 </template>
 
 <script>
-    export default {
-        name: 'TopContact'
-    }
+export default {
+  name: "TopContact",
+  data() {
+    return {
+      topContactD: require("@/assets/images/contact/desktop/image-hero.jpg"),
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
