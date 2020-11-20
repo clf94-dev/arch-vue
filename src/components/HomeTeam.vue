4fb6035f0e4c2dab1e7604dc9269083ae359059b
clf94-dev <clf94@hotmail.es>
2020-11-20 08:21:05 +0100

    added HomeTeam style

--- src/components/HomeTeam.vue
+++ src/components/HomeTeam.vue
@@ -1,13 +1,14 @@
 <template>
-  <b-container fluid>
-    <b-col class="team-cont">
-      <img :src="teamD" alt="team" />
-      <b-col class="team-text">
-        <h3>Small team, big ideas</h3>
-        <button @click="goToAbout()">
-          About Us <img :src="arrow" alt="arrow" />
-        </button>
-      </b-col> </b-col
+  <b-container fluid class="team-cont">
+    <b-row>
+      <b-col xs="12" sm="12" md="12" lg="12">
+        <img class="back-img" :src="teamD" alt="team" />
+        <div class="team-text">
+          <h3>Small team, big ideas</h3>
+          <button @click="goToAbout()">
+            About Us <img :src="arrow" alt="arrow" />
+          </button>
+        </div> </b-col></b-row
   ></b-container>
 </template>
 
@@ -28,4 +29,63 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.team-cont {
+  padding: 5% 7%;
+}
+
+.team-cont .team-text {
+  padding: 10% 15%;
+  text-align: start !important;
+  z-index: 2 !important;
+  width: 60%;
+}
+.team-cont .back-img {
+  width: 80%;
+  position: absolute !important;
+  left: 7%;
+  z-index: -1;
+}
+.team-cont .team-text h3 {
+  font-weight: 300;
+  color: white;
+  padding-right: 30% !important;
+  font-size: 40px;
+  padding-bottom: 30px;
+}
+
+@media screen and (min-width: 1080px) {
+  .team-cont .team-text h3 {
+    padding-right: 40%;
+    font-size: 56px;
+  }
+}
+
+@media screen and (min-width: 756px) {
+  .team-cont .team-text h3 {
+    font-size: 56px;
+  }
+}
+
+.team-cont .team-text button {
+  width: 250px;
+  height: 60px;
+  background-color: #000000;
+  color: white;
+  cursor: pointer;
+  align-items: center !important;
+  border: 1px solid black;
+}
+
+.team-cont .team-text button img {
+  padding-top: 5px;
+  padding-left: 20px;
+  height: 20px;
+}
+
+.team-cont .team-text button:hover {
+  background-color: #707070;
+  color: white;
+  border-color: #707070;
+}
+</style>
