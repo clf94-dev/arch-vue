1c6f6da19b4b34915286e599e20735a3aa8e1e51
clf94-dev <clf94@hotmail.es>
2020-11-20 07:32:16 +0100

    added navbar component struct

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-<h1> navbar component</h1>
+  <nav class="navbar">
+    <div class="navbar-cont">
+      <div class="navbar-logo">
+        <img :src="logo" alt="PhotoSnap" @click="goToHome()" />
+      </div>
+      <div class="menu-icon">
+        <i class="fas fa-bars"></i>
+      </div>
+      <ul class="nav-menu">
+        <li class="nav-item">
+          <router-link to="/portfolio" class="nav-links">Portfolio</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-links">About Us</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-links">Contact </router-link>
+        </li>
+      </ul>
     </div>
+  </nav>
 </template>
 
 <script>
-    export default {
-        name: 'Navbar'
-    }
+export default {
+  name: "Navbar",
+  methods: {
+    goToHome() {
+      this.$router.push("/home");
+    },
+  },
+  data() {
+    return {
+      logo: require("@/assets/images/logo.svg"),
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
