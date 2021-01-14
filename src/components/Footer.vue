41482de9db422fe9fca831567fa3c672a13c2413
clf94-dev <clf94@hotmail.es>
2021-01-14 09:46:20 +0100

    added footer struct

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-<h1>footer component</h1>
-    </div>
+  <b-container fluid class="footer-cont">
+    <b-row>
+      <b-col xs="12" sm="12" md="2" lg="2">
+        <img :src="archLogoW" alt="Arch" />
+      </b-col>
+      <b-col xs="12" sm="12" md="2" lg="2">
+        <a @click="goToPortfolio()">Portfolio</a>
+      </b-col>
+      <b-col xs="12" sm="12" md="2" lg="2">
+        <a @click="goToAbout()">About</a>
+      </b-col>
+      <b-col xs="12" sm="12" md="2" lg="2">
+        <a @click="goToContact()">Portfolio</a>
+      </b-col>
+      <b-col xs="12" sm="12" md="2" lg="2">
+        <button @click="goToPortfolio()">See Our Portfolio</button>
+      </b-col>
+      <b-col xs="12" sm="12" md="2" lg="2"> </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-    export default {
-        name: 'Footer'
-    }
+export default {
+  name: "Footer",
+  data() {
+    return {
+      archLogoW: require("@/assets/images/logo copy.svg"),
+    };
+  },
+  methods: {
+    goToHome() {
+      this.$router.push("/home");
+    },
+    goToPorfolio() {
+      this.$router.push("/portfolio");
+    },
+    goToAbout() {
+      this.$router.push("/about");
+    },
+    goToContact() {
+      this.$router.push("/contact");
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
