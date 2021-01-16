428729d7edc52decc44907ae658c3be36c8edfa1
clf94-dev <clf94@hotmail.es>
2021-01-16 10:48:15 +0100

    form basic struct

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,15 +1,25 @@
 <template>
-    <div>
-<h1> form component</h1>
-    </div>
+  <b-container fluid>
+    <b-row class="form-cont">
+      <b-col class="title-col" xs="12" sm="12" md="6" lg="6">
+        <h3>Connect with us</h3>
+      </b-col>
+      <b-col class="form-col" xs="12" sm="12" md="6" lg="6">
+        <form>
+          <input type="text" />
+          <input type="text" />
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+          <button></button>
+        </form>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-    export default {
-        name: 'Form'
-    }
+export default {
+  name: "Form",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
