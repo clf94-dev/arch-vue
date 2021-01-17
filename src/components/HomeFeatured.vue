01db6a2a7f1382244884ae61aca99e7fafd1dedb
clf94-dev <clf94@hotmail.es>
2021-01-17 09:42:20 +0100

    added data featured

--- src/components/HomeFeatured.vue
+++ src/components/HomeFeatured.vue
@@ -1,15 +1,48 @@
 <template>
-    <div>
-<h1> homefeatured component</h1>
-    </div>
+  <div>
+    <h1>homefeatured component</h1>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'HomeFeatured'
-    }
+export default {
+  name: "HomeFeatured",
+  data() {
+    return {
+      homefeatured: [
+        {
+          title: "Project Del Sol",
+          text: "",
+          srcD: require("@/assets/images//portfolio/desktop/image-del-sol.jpg"),
+          srcT: require("@/assets/images//portfolio/tablet/image-del-sol.jpg"),
+          srcM: require("@/assets/images//portfolio/mobile/image-del-sol.jpg"),
+          imgAlt: "Project Del Sol",
+        },
+        {
+          title: "228B Tower",
+          text: "",
+          srcD: require("@/assets/images/portfolio/desktop/image-228b.jpg"),
+          srcT: require("@/assets/images/portfolio/tablet/image-228b.jpg"),
+          srcM: require("@/assets/images/portfolio/tablet/image-228b.jpg"),
+          imgAlt: "228B Tower",
+        },
+        {
+          title: "Le Prototype",
+          text: "",
+          srcD: require("@/assets/images/portfolio/desktop/image-prototype.jpg"),
+          srcT: require("@/assets/images/portfolio/tablet/image-prototype.jpg"),
+          srcM: require("@/assets/images/portfolio/mobile/image-prototype.jpg"),
+          imgAlt: "Le Prototype",
+        },
+      ],
+    };
+  },
+  methods: {
+    goToPortfolio() {
+      this.$router.push("/portfolio");
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
